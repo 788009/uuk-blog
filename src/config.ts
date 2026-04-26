4b90a4e57e5f8b7bb3f6bb492902ef0580e086ee
788009 <2586738452@qq.com>
2026-04-26 11:25:45 +0800

    更新目录样式，清理多余注释

--- src/config.ts
+++ src/config.ts
@@ -29,8 +29,8 @@ export const siteConfig: SiteConfig = {
 		enable: true, // Display the table of contents on the right side of the post
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 		showConditions: {
-			startsWith: ["/posts/", "/records/"], // 匹配以这些路径开头的页面
-			equalsTo: [], // 精确匹配这些路径
+			startsWith: ["/posts/", "/records/"], // Matches pages starting with these paths
+			equalsTo: [], // Matches these paths exactly
 		},
 	},
 	favicon: [
