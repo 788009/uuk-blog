abd9e1651bc01c88f93ea2e238bcbfc5cfa8c4c8
788009 <2586738452@qq.com>
2026-05-14 23:43:10 +0800

    fix

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,4 +1,5 @@
 import rss from "@astrojs/rss";
+import { getSortedCollections } from "@utils/content-utils";
 import { url } from "@utils/url-utils";
 import type { APIContext } from "astro";
 import MarkdownIt from "markdown-it";
